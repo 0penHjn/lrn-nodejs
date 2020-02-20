0cb1e0fd53a453513fbfffc40ef78a170ae66b13
karimovnn <nail.karimov@domru.ru>
2020-02-20 14:03:27 +0300

    [nodejs-bootcamp] - 02_021

--- node_farm/index.js
+++ node_farm/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
@@ -9,6 +11,9 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'ut
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName,{lower:true}));
+console.log(slugs);
 
 const server = http.createServer((req,res) =>{
     console.log(req.url);
